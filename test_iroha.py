31843a496874bd9584fcc5d900ced170f6f809e4
Emil Melnikov <emilmelnikov@gmail.com>
2017-03-16 16:47:07 +0300

    Add learning test for flatbuffers API

--- test_iroha.py
+++ test_iroha.py
@@ -1,6 +1,11 @@
 import codecs
+import io
+
+import flatbuffers
 
 import iroha
+import _generated.iroha.BaseObject as base_object
+import _generated.iroha.BaseObjectType as base_object_type
 
 
 def test_create_key_pair():
@@ -33,3 +38,26 @@ def test_sha3_512():
     h = ('a69f73cca23a9ac5c8b567dc185a756e97c982164fe25859e0d1dcc1475c80a6'
          '15b2123af1f5f94c11e3e9402c3ac558f500199d95b6d3e301758586281dcd26')
     assert iroha.sha3_512(b'') == codecs.decode(h, 'hex')
+
+
+def test_flatbuffers_base_object():
+    # Build the byte buffer.
+    b = flatbuffers.Builder(0)
+    name = b.CreateString('treasure')
+    base_object.BaseObjectStart(b)
+    base_object.BaseObjectAddName(b, name)
+    base_object.BaseObjectAddType(b, base_object_type.BaseObjectType.Integer)
+    base_object.BaseObjectAddInteger(b, 7777777)
+    baseobjptr = base_object.BaseObjectEnd(b)
+    b.Finish(baseobjptr)
+    baseobjbytes = b.Output()
+
+    # Read the object from the byte buffer.
+    f = io.BytesIO(baseobjbytes)
+    buf = f.read()
+    obj = base_object.BaseObject.GetRootAsBaseObject(buf, 0)
+
+    # Object values should be equal to the written ones.
+    assert obj.Name() == b'treasure'
+    assert obj.Type() == base_object_type.BaseObjectType.Integer
+    assert obj.Integer() == 7777777
