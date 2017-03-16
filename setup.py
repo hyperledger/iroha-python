858d1fc58e575f367beda8a578477d0559589251
Emil Melnikov <emilmelnikov@gmail.com>
2017-03-16 16:48:12 +0300

    Add flatbuffers schema generation command

--- setup.py
+++ setup.py
@@ -1,7 +1,31 @@
 """Setup script."""
 
 
+import subprocess
+
 from setuptools import find_packages, setup
+from distutils.cmd import Command
+
+
+def xrun(*args):
+    subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                   check=True)
+
+
+class GenerateFlatbuffersCommand(Command):
+    description = 'generate FlatBuffers schema'
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        xrun('flatc', '--python', '-o', '_generated', 'api.fbs')
+        xrun('touch', '_generated/__init__.py')
 
 
 setup(
@@ -19,4 +43,7 @@ setup(
         'pysha3',
         'six',
     ],
+    cmdclass={
+        'genfbs': GenerateFlatbuffersCommand,
+    },
 )
