ea51a3b8c549c72eb2f791754d1c660e77eb257d
Emil Melnikov <emilmelnikov@users.noreply.github.com>
2017-03-28 05:09:34 +0300

    Add FlatBuffers support (#8)
    * Add example Python installation steps in README
    
    * Replace grpcio with flatbuffers package in setup.py
    
    * Import flatbuffers schema from Iroha
    
    * Delete protobuf-generated files
    
    * Delete gRPC-related code
    
    * Rename None to NoneType in flatbuffers schema
    
    "None" is a reserved word in Python.
    Evaluating the generated module results in error.
    
    * Add learning test for flatbuffers API
    
    * Add flatbuffers schema generation command
    
    * Describe how to generate flatbuffers schema
    
    * Add future imports in setup.py

--- iroha.py
+++ iroha.py
@@ -10,13 +10,7 @@ from __future__ import (absolute_import, division,
 from collections import namedtuple
 
 import ed25519
-import grpc
 import sha3
-
-from protos.api_pb2 import Asset,BaseObject,Query
-import protos.api_pb2_grpc as api_pb2_grpc
-
-from transaction import TransactionBuilder
 
 
 KeyPair = namedtuple('KeyPair', ['private_key', 'public_key'])
@@ -62,63 +56,3 @@ def sha3_384(message):
 
 def sha3_512(message):
     return sha3.sha3_512(message).digest()
-
-
-def getAccountInfo(publicKey):
-    query = Query(
-        senderPubkey=publicKey,
-        type="account"
-    )
-
-
-class Kannagi:
-    def __init__(self):
-        # ToDo configurable
-        channel = grpc.insecure_channel('172.17.0.3:50051')
-        self.sumeragi_stub = api_pb2_grpc.SumeragiStub(channel)
-        self.asset_repo_stub = api_pb2_grpc.AssetRepositoryStub(channel)
-        self.tx_repo_stub = api_pb2_grpc.TransactionRepositoryStub(channel)
-
-    def send(self, sender, receiver, name, value):
-        asset = Asset(
-            publicKey=sender,
-            name=name,
-            value={
-                "value":value
-            }
-        )
-        tx = TransactionBuilder("transfer", sender, receiver).set_asset(asset).build()
-        self.torii(tx)
-
-    def torii(self, tx):
-        res = self.sumeragi_stub.Torii(
-            tx
-        )
-        # ToDo  log class
-        print("client received: " + res.message)
-        return res
-
-    def get_account_info(self, publicKey):
-        query = Query(
-            senderPubkey=publicKey,
-            type="account"
-        )
-        return self.asset_repo_stub.find(query)
-
-    def get_asset_info(self, publicKey, assetName):
-        query = Query(
-            senderPubkey=publicKey,
-            type="asset",
-            value={
-                "name": BaseObject(valueString=assetName)
-            }
-        )
-        return self.asset_repo_stub.find(query)
-
-    def assetRepository(self, query):
-        return self.asset_repo_stub.find(query)
-
-    def get_all_transation(self):
-        # in now, don't use query
-        query = Query()
-        return self.tx_repo_stub.find(query)
--- setup.py
+++ setup.py
@@ -1,7 +1,31 @@
-"""Setup script."""
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
 
+import subprocess
 
 from setuptools import find_packages, setup
+from distutils.cmd import Command
+
+
+def xrun(*args):
+    subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                   check=True)
+
+
+class GenerateFlatbuffersCommand(Command):
+    description = 'generate FlatBuffers schema'
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        xrun('flatc', '--python', '-o', '_generated', 'api.fbs')
+        xrun('touch', '_generated/__init__.py')
 
 
 setup(
@@ -14,9 +38,12 @@ setup(
     # Keep the dependencies lexicographically sorted.
     install_requires=[
         'ed25519',
+        'flatbuffers',
         'future',
-        'grpcio',
         'pysha3',
         'six',
     ],
+    cmdclass={
+        'genfbs': GenerateFlatbuffersCommand,
+    },
 )
