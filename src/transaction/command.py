e590a932f39c20bdab1c3e38cdd1fcdf7f96a743
satellitex <s.a.t.e.3.ths@gmail.com>
2017-09-28 19:43:35 +0900

    command parse test passed

---
+++ src/transaction/command.py
@@ -0,0 +1,22 @@
+from schema.commands_pb2 import Command
+
+def wrap_cmd(cmd):
+    if type(cmd) == type(Command.CreateAccount()):
+        return Command(create_account = cmd)
+    elif type(cmd) == type(Command.AddSignatory()):
+        return Command(add_signatory = cmd)
+    elif type(cmd) == type(Command.RemoveSignatory()):
+        return Command(remove_signaotory = cmd)
+    elif type(cmd) == type(Command.SetAccountQuorum()):
+        return Command(set_account_quorum = cmd)
+    elif type(cmd) == type(Command.CreateAsset()):
+        return Command(create_asset = cmd)
+    elif type(cmd) == type(Command.CreateDomain()):
+        return Command(create_domain = cmd)
+    elif type(cmd) == type(Command.AddAssetQuantity()):
+        return Command(add_asset_quantity = cmd)
+    elif type(cmd) == type(Command.TransferAsset()):
+        return Command(transfer_asset = cmd)
+    else:
+        # TODO throw except
+        return False
