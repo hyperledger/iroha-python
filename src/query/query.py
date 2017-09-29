285315e7c2ef6a77587ca92643b9b210e8708ebe
satellitex <s.a.t.e.3.ths@gmail.com>
2017-09-29 14:58:18 +0900

    add Query class
    Signed-off-by: satellitex <s.a.t.e.3.ths@gmail.com>

---
+++ src/query/query.py
@@ -0,0 +1,44 @@
+from schema.response_pb2 import Query as QuerySchema
+
+from src.helper import logger, crypto, stateless_validator
+from src.primitive.signatories import Signatories
+
+class Query:
+    def __init__(self):
+        logger.debug("Create Query Construct")
+        self.query = QuerySchema(
+            payload = QuerySchema.Payload(
+                created_time = crypto.now()
+            )
+            # TODO query has signature
+        )
+        self.signatories = Signatories()
+
+
+    def set_creator_account_id(self,creator_account_id):
+        logger.debug("Query.set_creator_account_id")
+        self.query.payload.creator_account_id = creator_account_id
+
+    def set_query_counter(self,query_counter):
+        logger.debug("Query.set_tx_counter")
+        self.query.payload.query_counter = query_counter
+
+    def time_stamp(self):
+        logger.debug("Query.time_stamp")
+        self.query.payload.created_time = crypto.now()
+
+    def hash(self):
+        logger.debug("Query.hash")
+        return crypto.sign_hash(self.query.payload)
+
+    def verify(self):
+        logger.debug("Query.verify")
+        return stateless_validator.query(self.query)
+
+    def set_request(self,cmd):
+        logger.debug("set_request")
+
+
+    def debug_proto_query(self):
+        logger.debug("debug_porto_query")
+        return self.query
