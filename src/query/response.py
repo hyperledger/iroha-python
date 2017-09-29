38bb9d9b92d69cbc7f05b25cfdc57ddb5293d6e4
satellitex <s.a.t.e.3.ths@gmail.com>
2017-09-29 18:25:48 +0900

    add test_response
    Signed-off-by: satellitex <s.a.t.e.3.ths@gmail.com>

--- src/query/response.py
+++ src/query/response.py
@@ -16,55 +16,61 @@ class Response:
 
     def has_account(self):
         logger.debug("Response.has_account")
-        return self.response.HasField("account_response")
+        return self.response.payload.HasField("account_response")
 
     def account(self):
         logger.debug("Response.account_response")
         if self.has_account():
-            return self.response.account_response.account
+            return self.response.payload.account_response.account
         else:
             raise NotAccountResponseException
 
     def has_account_asset(self):
         logger.debug("Response.has_account_asset")
-        return self.response.HasField("account_assets_response")
+        return self.response.payload.HasField("account_assets_response")
 
     def account_asset(self):
         logger.debug("Response.account_asset")
         if self.has_account_asset():
-            return self.response.account_assets_response.account_asset
+            return self.response.payload.account_assets_response.account_asset
         else:
             raise NotAccountAssetResponseException
 
     def has_signatories(self):
         logger.debug("Response.has_signatories")
-        return self.response.HasField("signatories_response")
+        return self.response.payload.HasField("signatories_response")
 
     def signatories(self):
         logger.debug("Response.signatories")
         if self.has_signatories():
-            return self.response.signatories_response.keys
+            keys = []
+            for k in self.response.payload.signatories_response.keys:
+                keys.append(k)
+            return keys
         else:
             raise NotSignatoriesResponseExcepiton
 
     def has_transactions(self):
         logger.debug("Response.has_transactions")
-        return self.response.HasField("transactions_response")
+        return self.response.payload.HasField("transactions_response")
 
     def transactions(self):
         logger.debug("Response.transactions")
         if self.has_transactions():
-            return self.response.transactions_response.transactions
+            txs = []
+            for tx in self.response.payload.transactions_response.transactions:
+                txs.append(tx)
+            return txs
         else:
             raise NotTransactionsResponseException
 
     def has_error(self):
         logger.debug("Response.has_error")
-        return self.response.HasField("error_response")
+        return self.response.payload.HasField("error_response")
 
     def error_response(self):
         logger.debug("Response.error_response")
         if self.has_error():
-            return self.response.error_response
+            return self.response.payload.error_response
         else:
             raise NotErrorResponseException
