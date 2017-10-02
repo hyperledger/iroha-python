ecef600536971c59c0ba49733ec04b03a3dc4784
satellitex <s.a.t.e.3.ths@gmail.com>
2017-10-02 16:52:29 +0900

    add set_connection

--- src/creator.py
+++ src/creator.py
@@ -9,19 +9,26 @@ from src.transaction.transaction import Transaction
 from src.query.query import Query
 
 class Creator:
-    def __init__(self):
+    def __init__(self,account_id, keys, connection):
         logger.info("Construct Creator")
+        self.set_account_id(account_id)
+        self.set_keys(keys)
+        self.set_connection(connection)
 
     def set_account_id(self, account_id):
         logger.debug("Creator.set_account_id")
         self.creator_account_id = account_id
 
     def set_keys(self, keys):
-        logger.debug("set_keys")
+        logger.debug("Creator.set_keys")
         self.signatories = keys
 
+    def set_connection(self,connection):
+        logger.debug("Creator.set_connection")
+        self.connection = connection
+
     def create_tx(self):
-        logger.debug("create_tx")
+        logger.debug("Creator.create_tx")
         retx = Transaction()
         retx.add_key_pairs(self.signatories)
         retx.set_creator_account_id(self.creator_account_id)
@@ -29,7 +36,7 @@ class Creator:
         return retx
 
     def create_query(self):
-        logger.debug("create_query")
+        logger.debug("Creator.create_query")
         retq = Query()
         retq.set_creator_account_id(self.creator_account_id)
         retq.time_stamp()
