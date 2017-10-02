74e9e5cd19893c5361ea5a3ec028855187ca8e51
satellitex <s.a.t.e.3.ths@gmail.com>
2017-10-02 18:01:33 +0900

    creator add connect (and docs

--- src/creator.py
+++ src/creator.py
@@ -9,35 +9,81 @@ from src.transaction.transaction import Transaction
 from src.query.query import Query
 
 class Creator:
-    def __init__(self,account_id, keys, connection):
+    """
+    Creator is Account for creating transaction or query.
+    Creator has account_id, signatories and connection(used to connect iroha).
+    """
+    def __init__(self,account_id=None, keys=None, connection=None):
+        """
+        Initialize Creator info
+
+        Args:
+            account_id ( str ): creator's account_id (e.g : "account@domain.com")
+            keys ( []`KeyPair` ): array of `KeyPair`. There are signatories of this creator.
+            connection ( `Connection` ): connection used to connect iroha.
+        """
         logger.info("Construct Creator")
-        self.set_account_id(account_id)
-        self.set_keys(keys)
-        self.set_connection(connection)
+        if account_id:
+            self.set_account_id(account_id)
+        if keys:
+            self.set_keys(keys)
+        if connection:
+            self.set_connection(connection)
 
     def set_account_id(self, account_id):
+        """
+        Set creator account id
+
+        Args:
+            account_id ( str ): creator account id
+        """
         logger.debug("Creator.set_account_id")
         self.creator_account_id = account_id
 
     def set_keys(self, keys):
+        """
+        Set keypairs (means signatoiers) of creator
+        Args:
+            keys ( []`KeyPair` ): array of keypair ( public key and private key )
+        """
         logger.debug("Creator.set_keys")
         self.signatories = keys
 
     def set_connection(self,connection):
+        """
+        Set Connection used to connect iroha
+        Args:
+            connection ( `Connect` ): connection used to connect iroha
+        """
         logger.debug("Creator.set_connection")
         self.connection = connection
 
     def create_tx(self):
+        """
+        Create Transaction
+
+        Returns:
+            `Transaction`: created transaction with added signatory, account_id, time stamp, and connection
+        """
         logger.debug("Creator.create_tx")
         retx = Transaction()
+        retx.set_connection(self.connection)
         retx.add_key_pairs(self.signatories)
         retx.set_creator_account_id(self.creator_account_id)
         retx.time_stamp()
         return retx
 
     def create_query(self):
+        """
+        Create Query
+
+        Returns:
+            `Query`: created query with added account_id, tima stamp, and connection.
+
+        """
         logger.debug("Creator.create_query")
         retq = Query()
+        retq.set_connection(self.connection)
         retq.set_creator_account_id(self.creator_account_id)
         retq.time_stamp()
         return retq
