ca4b17c80d5f4d8b5943387a75b11292e9ea39fe
satellitex <s.a.t.e.3.ths@gmail.com>
2017-10-02 18:47:33 +0900

    test initialize connection

---
+++ tests/network/test_connection.py
@@ -0,0 +1,23 @@
+import unittest
+
+from src.network.connection import Connection
+from src.helper import logger
+
+class ConnectionTest(unittest.TestCase):
+
+    def setUp(self):
+        logger.setDebug()
+        logger.info("ConnectionTest")
+
+    def test_connect_normal(self):
+        try:
+            Connection(ip="127.162.19.19",port="5050")
+        except:
+            self.assertTrue(False)
+        self.assertTrue(True)
+
+    def test_connect_except(self):
+        try:
+            Connection(ip="hostname",port="5050")
+        except:
+            self.assertTrue(True)
