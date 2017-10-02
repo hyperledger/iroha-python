3c7a9a6739e4fc3b9cc7f32d93305cb32fd1ff16
satellitex <s.a.t.e.3.ths@gmail.com>
2017-10-02 17:36:33 +0900

    add transaction;

--- src/transaction/transaction.py
+++ src/transaction/transaction.py
@@ -19,58 +19,112 @@ class Transaction:
 
 
     def set_creator_account_id(self,creator_account_id):
+        """
+        Set creator of this transaction.
+
+        Args:
+            creator_account_id ( str ) : it is creator's account id of transaction
+        """
         logger.debug("Transaction.set_creator_account_id")
         self.tx.payload.creator_account_id = creator_account_id
 
     def set_tx_counter(self,tx_counter):
+        """
+        Set tx counter of this transaction
+
+        Args:
+            tx_counter ( int ) : tx_counter is counter of transaction
+        """
         logger.debug("Transaction.set_tx_counter")
         self.tx.payload.tx_counter = tx_counter
 
     def set_connection(self,connection):
+        """
+        Set connection of this transaction
+
+        Args:
+            connection ( `Connection` ): connection is connector of transaction
+        """
         logger.debug("Transaction.set_connection")
         self.connection = connection
 
     def add_key_pair(self,keypair):
+        """
+        Add keypair ( public key and private key generated iroha.keygen() )
+
+        Args:
+            keypair ( `KeyPair` ) : keypair generated iroha.keygen()
+        """
         logger.debug("Transaction.add_key_pair")
         self.signatories.append(keypair)
 
     def add_key_pairs(self,keypairs):
+        """
+        Add keypairs ( public key and private key generated iroha.keygen() )
+        Args:
+            keypairs ( []`KeyPair` ): array of keypair
+        """
         logger.debug("Transaction.add_key_pairs")
         for k in keypairs:
             self.signatories.append(k)
 
     def time_stamp(self):
+        """
+        Set current(call this function) time timestamp of this transaction
+        """
         logger.debug("Transaction.time_stamp")
         self.tx.payload.created_time = crypto.now()
 
     def sign(self):
+        """
+        Do sign to this transaction from this signatories.
+        """
         logger.debug("Transaction.sign")
         self.signatories.sign(self.tx)
         self.signatories.clean()
 
     def signatures_clean(self):
+        """
+        Delete all signature info. ( So, after call this func number of signatures of transaction is 0 )
+        """
         logger.debug("Transaction.signatures_clean")
         while self.tx.signatures.__len__():
             self.tx.signatures.pop()
 
     def count_signatures(self):
+        """
+        Count all signatures.
+        Returns:
+            int: number of this transactions's signatures
+
+        """
         logger.debug("Transaction.count_signatures")
         return self.tx.signatures.__len__()
 
     def signatories_clean(self):
+        """
+        Delete all signatories info. ( not delete signatures of transaction info )
+        """
         logger.debug("Transaction.signatories_clean")
         self.signatories.clean()
 
     def hash(self):
+        """
+        Get hash of this transaction
+
+        Returns:
+            bytes: The return value is hash of this transction
+        """
         logger.debug("Transaction.hash")
         return crypto.sign_hash(self.tx.payload)
 
     def verify(self):
         """
-        Stateless validate this transaction.
+        Verify stateless validate this transaction.
+        Included signature check.
 
         Returns:
-            bool: 
+            bool: The return value. True for success, False otherwise.
 
         """
         logger.debug("Transaction.verify")
@@ -116,5 +170,11 @@ class Transaction:
         self.connection.tx_stub().Torii(self.tx)
 
     def check(self):
+        """
+        Check this transaction status for iroha
+        Returns:
+            TxStatusResponse: Transaction Status Code
+        """
         logger.debug("Transaction.check")
-        # TODO
+        # TODO thinking now
+        return self.connection.tx_stub().Status(self.hash())
