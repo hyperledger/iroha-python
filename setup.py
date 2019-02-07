6699b09e33fc5167f963ebd2fcab84f5b9e08b22
Igor Egorov <igor@soramitsu.co.jp>
2019-02-07 19:05:50 +0300

    Init commit
    Signed-off-by: Igor Egorov <igor@soramitsu.co.jp>

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open('README.md', 'r') as readme_file:
+    long_description = readme_file.read()
+
+setuptools.setup(
+    name='iroha',
+    version='0.0.1',
+    description='Python library for Hyperledger Iroha',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='Apache-2.0',
+    url='https://github.com/hyperledger/iroha-python',
+    packages=setuptools.find_packages(
+        exclude=['dist', 'build', '*.pyc', '.DS_Store',
+                 '.vscode',  '.idea', '__pycache__', '*.bak']),
+    install_requires=[
+        'grpcio-tools',
+        'pysha3;python_version<"3.6"'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent'
+    ]
+)
