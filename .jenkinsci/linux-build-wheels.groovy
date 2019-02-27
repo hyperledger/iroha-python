8cd86be2496ea720632f8ecc40e03f58f17077cd
Alexey Rodionov <rodionov12@gmail.com>
2019-02-27 10:57:36 +0300

    test builds
    Signed-off-by: Alexey Rodionov <rodionov12@gmail.com>

--- .jenkinsci/linux-build-wheels.groovy
+++ .jenkinsci/linux-build-wheels.groovy
@@ -1,4 +1,4 @@
-  #!/usr/bin/env groovy
+#!/usr/bin/env groovy
 
   def doPythonWheels() {
     version = sh(script: 'git describe --tags \$(git rev-list --tags --max-count=1)', returnStdout: true).trim()
