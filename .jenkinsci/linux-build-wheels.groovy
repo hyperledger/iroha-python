3bd28a33387535fcf4ae2b025bcafb6fd1e38aee
Alexey Rodionov <rodionov12@gmail.com>
2019-02-27 12:30:29 +0300

    test
    Signed-off-by: Alexey Rodionov <rodionov12@gmail.com>

--- .jenkinsci/linux-build-wheels.groovy
+++ .jenkinsci/linux-build-wheels.groovy
@@ -19,14 +19,15 @@ def doPythonWheels() {
 def publishWheels() {
     checkTag = sh(script: 'git describe --tags --exact-match ${GIT_COMMIT}', returnStatus: true)
     // if (env.GIT_LOCAL_BRANCH == "master" &&  checkTag)
-    iC = docker.image('quay.io/pypa/manylinux1_x86_64')
-    iC.inside("") {
-        // sh(script: '/opt/python/cp35-cp35m/bin/pip install twine', returnStdout: true)
-        withCredentials([usernamePassword(credentialsId: 'ci_nexus', passwordVariable: 'CI_NEXUS_PASSWORD', usernameVariable: 'CI_NEXUS_USERNAME')]) {
-            sh(script: "find wheelhouse -type f -name \"iroha*.whl\" -exec curl -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file wheelhouse/{} https://nexus.iroha.tech/repository/artifacts/iroha-python/wheels/{} \;")
-            // sh "/opt/python/cp35-cp35m/bin/twine upload --skip-existing -u ${CI_NEXUS_USERNAME} -p ${CI_NEXUS_PASSWORD} --repository-url https://nexus.soramitsu.co.jp/repository/pypi-upload-release/ wheelhouse/iroha*.whl"
-        }
+    withCredentials([usernamePassword(credentialsId: 'ci_nexus', passwordVariable: 'CI_NEXUS_PASSWORD', usernameVariable: 'CI_NEXUS_USERNAME')]) {
+        sh(script: "find wheelhouse -type f -name \"iroha*.whl\" -exec curl -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file wheelhouse/{} https://nexus.iroha.tech/repository/artifacts/iroha-python/wheels/{} \\;")
+        // sh "/opt/python/cp35-cp35m/bin/twine upload --skip-existing -u ${CI_NEXUS_USERNAME} -p ${CI_NEXUS_PASSWORD} --repository-url https://nexus.soramitsu.co.jp/repository/pypi-upload-release/ wheelhouse/iroha*.whl"
     }
+    // iC = docker.image('quay.io/pypa/manylinux1_x86_64')
+    // iC.inside("") {
+    //     // sh(script: '/opt/python/cp35-cp35m/bin/pip install twine', returnStdout: true)
+
+    // }
 }
 
 return this
