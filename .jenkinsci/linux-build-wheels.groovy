65b5d7711ec84eb9103144c1d39c2183c36fac63
Alexey Rodionov <rodionov12@gmail.com>
2019-02-28 10:34:52 +0300

    fix
    Signed-off-by: Alexey Rodionov <rodionov12@gmail.com>

--- .jenkinsci/linux-build-wheels.groovy
+++ .jenkinsci/linux-build-wheels.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def doPythonWheels() {
-    version = sh(script: 'git describe --tags \$(git rev-list --tags --max-count=1) || true', returnStdout: true).trim()
+    version = sh(script: 'git describe --tags \$(git rev-list --tags --max-count=1) || "0.0.1"', returnStdout: true).trim()
     repo = 'release'
     if (env.GIT_LOCAL_BRANCH != "master") {
         repo = 'develop'
@@ -20,7 +20,7 @@ def publishWheels() {
     checkTag = sh(script: 'git describe --tags --exact-match ${GIT_COMMIT}', returnStatus: true)
     // if (env.GIT_LOCAL_BRANCH == "master" &&  checkTag)
     withCredentials([usernamePassword(credentialsId: 'ci_nexus', passwordVariable: 'CI_NEXUS_PASSWORD', usernameVariable: 'CI_NEXUS_USERNAME')]) {
-        sh(script: "find wheelhouse -type f -name \"iroha*.whl\" -exec curl -u ${CI_NEXUS_USERNAME}:${CI_NEXUS_PASSWORD} --upload-file wheelhouse/{} https://nexus.iroha.tech/repository/artifacts/iroha-python/wheels/{} \\;")
+        sh(script: "find wheelhouse -type f -name \"iroha*.whl\" -exec curl -u ${CI_NEXUS_USERNAME}:${CI_NEXUS_PASSWORD} --upload-file {} https://nexus.iroha.tech/repository/artifacts/iroha-python/{} \\;")
         // sh "/opt/python/cp35-cp35m/bin/twine upload --skip-existing -u ${CI_NEXUS_USERNAME} -p ${CI_NEXUS_PASSWORD} --repository-url https://nexus.soramitsu.co.jp/repository/pypi-upload-release/ wheelhouse/iroha*.whl"
     }
     // iC = docker.image('quay.io/pypa/manylinux1_x86_64')
