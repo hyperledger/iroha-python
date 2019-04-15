183be3ac106197911cbf5283fed6cb78ac4addfe
Igor Egorov <igor@soramitsu.co.jp>
2019-04-15 17:54:10 +0300

    rm -rf
    Signed-off-by: Igor Egorov <igor@soramitsu.co.jp>

--- iroha/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-if sys.version_info[0] < 3:
-    raise Exception(
-    	'Python 3 or a more recent version is required. Python 2 is not supported.')
-
-from .iroha import *
-name = 'iroha'
