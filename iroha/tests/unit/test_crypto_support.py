ab8872a7d5074010c27db4dcbe3b7e5f04db3d49
stlavr <lawrentievsv@gmail.com>
2020-07-21 17:54:59 +0300

    Fix data for test
      Signed-off-by: stlavr <lawrentievsv@gmail.com>
    
    Signed-off-by: Mikhail Boldyrev <miboldyrev@gmail.com>

--- iroha/tests/unit/test_crypto_support.py
+++ iroha/tests/unit/test_crypto_support.py
@@ -16,12 +16,8 @@ data_scope = ([Test_data(private_key="f101537e319568c765b2cc89698325604991dca57b
                          public_key=b'313a07e6384776ed95447710d15e59148473ccfc052a681317a72a69f2a49910'),
                Test_data(private_key=b'f101537e319568c765b2cc89698325604991dca57b9716b58016b253506cab70',
                          public_key=b'313a07e6384776ed95447710d15e59148473ccfc052a681317a72a69f2a49910'),
-               Test_data(
-                   private_key=b'\x99\xfe\x89i\xac\xda\xfb\t\xbf\xdd\x00F7\x0e/\xa2X\x0b\x0c%\x91\xa266%%\r\xa1Mw\x1bc'
-                               b'\xca\r7,\x15\xb7\x12\xb4o\xa1\xc6\xe4\xaf\xc4\xfd~#\xe9\x1d\xbf\x86\x9d\xa4\x97\xdb'
-                               b'\x89\x8d\x88OE\xac@',
-                   public_key='ed0120ca0d372c15b712b46fa1c6e4afc4fd7e23e91dbf869da497db898d884f45ac40',
-                   seed=b'\x99\xfe\x89i\xac\xda\xfb\t\xbf\xdd\x00F7\x0e/\xa2X\x0b\x0c%\x91\xa266%%\r\xa1Mw\x1bc')
+               Test_data(public_key='ed0120ca0d372c15b712b46fa1c6e4afc4fd7e23e91dbf869da497db898d884f45ac40',
+                         seed=b'\x99\xfe\x89i\xac\xda\xfb\t\xbf\xdd\x00F7\x0e/\xa2X\x0b\x0c%\x91\xa266%%\r\xa1Mw\x1bc')
                ])
 data_ids = ['priv_key, pub_key({},{})'.format(t.private_key, t.public_key)
             for t in data_scope]
