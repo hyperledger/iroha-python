579ab7cd5a3389bbd54b8c65e8a5c46208395e89
stlavr <lawrentievsv@gmail.com>
2020-07-27 20:49:22 +0300

    Fix key pair generation on tests
    Signed-off-by: Mikhail Boldyrev <miboldyrev@gmail.com>

--- iroha/tests/unit/test_crypto_support.py
+++ iroha/tests/unit/test_crypto_support.py
@@ -3,24 +3,18 @@
 from iroha import IrohaCrypto, ed25519_sha2
 
 
-def test_derive_public_key(data_for_crypto_test):
+def test_derive_public_key(crypto_data):
     """Checking call with different data types"""
-    if data_for_crypto_test.seed is not None:  # if seed is present in the data, then we create a key_pair out of it.
-        key_pair = ed25519_sha2.SigningKey(seed=data_for_crypto_test.seed)
-        public_key = IrohaCrypto.derive_public_key(key_pair)
-    else:
-        public_key = IrohaCrypto.derive_public_key(data_for_crypto_test.private_key)
-    assert public_key == data_for_crypto_test.public_key
+    public_key = IrohaCrypto.derive_public_key(crypto_data.private_key)
+    assert public_key == crypto_data.public_key
 
 
-def test_create_signature(data_for_crypto_test):
+def test_create_signature(crypto_data):
     """Checking call with different data types"""
-    if data_for_crypto_test.seed is not None:
-        key_pair = ed25519_sha2.SigningKey(seed=data_for_crypto_test.seed)
-        signature = IrohaCrypto._signature(data_for_crypto_test.message, key_pair)
-        validate = IrohaCrypto.is_sha2_signature_valid(data_for_crypto_test.message, signature)
+    signature = IrohaCrypto._signature(crypto_data.message, crypto_data.private_key)
+    if isinstance(crypto_data.private_key, ed25519_sha2.SigningKey):  # Use different validation method
+        validate = IrohaCrypto.is_sha2_signature_valid(crypto_data.message, signature)
         assert validate
     else:
-        signature = IrohaCrypto._signature(data_for_crypto_test.message, data_for_crypto_test.private_key)
-        validate = IrohaCrypto.is_signature_valid(data_for_crypto_test.message, signature)
+        validate = IrohaCrypto.is_signature_valid(crypto_data.message, signature)
         assert validate
