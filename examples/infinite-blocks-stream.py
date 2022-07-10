c937b62d976e2ff56c0d4cf3d5f42e05f4124878
G.Bazior <bazior@agh.edu.pl>
2022-07-10 21:24:59 +0200

    Small refactoring of infinite-blocks-stream.py
    Signed-off-by: G.Bazior <bazior@agh.edu.pl>

--- examples/infinite-blocks-stream.py
+++ examples/infinite-blocks-stream.py
@@ -8,6 +8,9 @@ from iroha import Iroha, IrohaGrpc
 from iroha import IrohaCrypto
 import sys
 import os
+import grpc  # grpc.RpcError
+from functools import wraps
+
 
 if sys.version_info[0] < 3:
     raise Exception('Python 3 or a more recent version is required.')
@@ -19,18 +22,19 @@ ADMIN_PRIVATE_KEY = os.getenv(
     'ADMIN_PRIVATE_KEY', 'f101537e319568c765b2cc89698325604991dca57b9716b58016b253506cab70')
 
 iroha = Iroha(ADMIN_ACCOUNT_ID)
-net = IrohaGrpc('{}:{}'.format(IROHA_HOST_ADDR, IROHA_PORT))
+net = IrohaGrpc(f'{IROHA_HOST_ADDR}:{IROHA_PORT}')
+
 
 def trace(func):
     """
     A decorator for tracing methods' begin/end execution points
     """
-
+    @wraps(func)
     def tracer(*args, **kwargs):
         name = func.__name__
-        print('\tEntering "{}"'.format(name))
+        print(f'\tEntering "{name}": {args}')
         result = func(*args, **kwargs)
-        print('\tLeaving "{}"'.format(name))
+        print(f'\tLeaving "{name}"')
         return result
 
     return tracer
@@ -45,7 +49,19 @@ def get_blocks():
     query = iroha.blocks_query()
     IrohaCrypto.sign_query(query, ADMIN_PRIVATE_KEY)
     for block in net.send_blocks_stream_query(query):
-        print('The next block arrived:', block)
+        print('\nThe next block arrived:', block)
 
 
-get_blocks()
+if __name__ == '__main__':
+    try:
+        print('The script will wait for new blocks. '
+              'You need to send some commands to the node to see any blocks coming')
+        get_blocks()
+    except grpc.RpcError as rpc_error:
+        if rpc_error.code() == grpc.StatusCode.UNAVAILABLE:
+            print(f'[E] Iroha is not running in address:'
+                  f'{IROHA_HOST_ADDR}:{IROHA_TLS_PORT}!')
+        else:
+            print(e)
+    except RuntimeError as e:
+        print(e)
