2cf48f8d33cb45971728b213904fee47e34b96ff
Sam H. Smith <sam.henning.smith@protonmail.com>
2024-05-13 17:16:54 +0200

    [fix] #140: Comprehensive key generation
    Signed-off-by: Sam H. Smith <sam.henning.smith@protonmail.com>

--- tests/helpers.py
+++ tests/helpers.py
@@ -5,4 +5,4 @@ def generate_public_key(seed="abcd1122"):
     """
     Generate a public key using Ed25519PrivateKey.
     """
-    return iroha.KeyGenConfiguration.default().use_seed_hex(seed).generate().public_key
+    return iroha.KeyPair.from_hex_seed(seed).public_key
